9bb863b9b214577f157040a201444b49ea776a9e
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2024-02-21 21:08:18 +0000

    Add files via upload

---
+++ 0x02 JS/01_Lesson-Introduction/script.js
@@ -0,0 +1,48 @@
+// This is a comment
+
+// DATA TYPES
+"Osborn"
+typeof "Osborn";                        // CONSOLE: string
+typeof 42;                              // CONSOLE: number
+typeof true;                            // CONSOLE: boolean
+
+
+// VARIABLES
+let myVariable;
+typeof myVariable;                      // undefined data type
+
+myVariable = "Osborn";
+typeof myVariable;                      // CONSOLE: string
+
+// MATHEMATICAL OPERATORS
+42 + 2;                                 // CONSOLE: 44
+42 - 2;                                 // CONSOLE: 40
+
+let myNumber = 42;
+myNumber + 2;                           // CONSOLE: 44
+myNumber * 2;                           // CONSOLE: 84
+myNumber / 2;                           // CONSOLE: 21
+myNumber = 44 * 2;                      // CONSOLE: 88
+myNumber;                               // CONSOLE: 88
+
+myNumber + myVariable;                  // CONSOLE: 88Osborn
+
+let myCombo = myNumber + myVariable;
+typeof myCombo;                         // CONSOLE: "string"
+
+
+myNumber + 2;                           // CONSOLE: 90
+
+
+
+// COMPARISON
+myVariable === myNumber                 // CONSOLE: false
+myNumber === 90;                        // CONSOLE: true
+myBool;                                 // CONSOLE: true
+
+myNumber !== 90;                        // CONSOLE: false
+myNumber >= 90;                         // CONSOLE: true
+myNumber < 90;                          // CONSOLE: false
+
+console.log("Hello World!");
+
---
+++ 0x02 JS/02_Lesson_Link-JS-To-HTML/js/main.js
@@ -0,0 +1,4 @@
+console.log("Hello World!");
+console.log(typeof "Osborn");
+console.log(typeof 42);
+console.log(typeof true);
